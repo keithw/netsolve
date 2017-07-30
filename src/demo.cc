381a21cd4e57c4e2addb80a86be8fa13be7fc292
Keith Winstein <keithw@cs.stanford.edu>
2017-07-30 06:47:21 -0700

    Differential logger

--- src/demo.cc
+++ src/demo.cc
@@ -2,10 +2,18 @@
 #include <iostream>
 #include <limits>
 #include <cmath>
+#include <iomanip>
 
 #include "parking_lot.hh"
 
 using namespace std;
+
+void print( const tuple<double, double, double> & best_rates )
+{
+  cout << "** Rates: " << get<0>( best_rates )
+       << " " << get<1>( best_rates )
+       << " " << get<2>( best_rates ) << "\n";  
+}
 
 double PCC_utility( const double sending_rate,
 		   const double throughput )
@@ -15,9 +23,31 @@ double PCC_utility( const double sending_rate,
 }
 
 double utility( const double sending_rate __attribute((unused)),
-	       const double throughput )
+		const double throughput )
 {
   return log( throughput );
+}
+
+template <unsigned int i>
+tuple<double, double, double> adjust_guess( tuple<double, double, double> guess,
+					    const double epsilon )
+{
+  get<i>( guess ) += epsilon;
+  get<i>( guess ) = max( 0.0, get<i>( guess ) );
+  return guess;
+}
+
+template <unsigned int i>
+double evaluate( ParkingLot & network,
+		 const tuple<double, double, double> guess )
+{
+  const auto throughputs = network.throughputs_fast( get<0>( guess ),
+						     get<1>( guess ),
+						     get<2>( guess ) );
+
+  const double score = PCC_utility( get<i>( guess ), get<i>( throughputs ) );
+
+  return score;
 }
 
 template <unsigned int i>
@@ -26,21 +56,10 @@ double partial( ParkingLot & network,
 		const double epsilon )
 {
   /* reference point */
-  const auto throughputs = network.throughputs_fast( get<0>( guess ),
-						     get<1>( guess ),
-						     get<2>( guess ) );
-
-  const double score = PCC_utility( get<i>( guess ), get<i>( throughputs ) );
+  const double score = evaluate<i>( network, guess );
 
   /* new point */
-  auto new_guess = guess;
-  get<i>( new_guess ) += epsilon;
-
-  const auto new_throughputs = network.throughputs_fast( get<0>( new_guess ),
-							 get<1>( new_guess ),
-							 get<2>( new_guess ) );
-
-  const double new_score = PCC_utility( get<i>( new_guess ), get<i>( new_throughputs ) );
+  const double new_score = evaluate<i>( network, adjust_guess<i>( guess, epsilon ) );
 
   /* partial derivative */
   return (new_score - score) / (epsilon);
@@ -48,43 +67,46 @@ double partial( ParkingLot & network,
 
 template <unsigned int i>
 tuple<double, double, double> search_one( ParkingLot & network,
-					  tuple<double, double, double> guess,
-					  const double search_radius )
+					  tuple<double, double, double> guess )
 {
-  tuple<double, double, double> best_rates { -1, -1, -1 };
-  double best_score = numeric_limits<double>::lowest();
+  //  cout << "Optimizing sender " << i << "\n";
 
-  const double the_min = max( 0.0, get<i>( guess ) - search_radius );
-  const double the_max = get<i>( guess ) + search_radius;
+  double delta = 256 * numeric_limits<double>::epsilon();
 
-  cout << "Optimizing sender " << i << " on [" << the_min << ".." << the_max << "].\n";
+  while ( true ) {
+    if ( delta >= 0.1 ) {
+      return guess;
+    }
 
-  cout << "Tiny increase d/dx: " << partial<i>( network, guess,
-						1024 * numeric_limits<double>::epsilon() );
+    const auto low_guess = adjust_guess<i>( guess, -delta );
+    const auto high_guess = adjust_guess<i>( guess, delta );
 
-  cout << ", large increase d/dx: " << partial<i>( network, guess,
-						   .0001 ) << "\n";
+    const double low_score = evaluate<i>( network, low_guess );
+    const double mid_score = evaluate<i>( network, guess );
+    const double high_score = evaluate<i>( network, high_guess );
 
+    if ( get<i>( guess ) >= 100 ) {
+      get<i>( guess ) *= 0.5;
+      cout << "Terminating search because flooding found.\n";
+      return guess;
+    }
 
-  for ( double val = the_min; val < the_max; val += (the_max - the_min) / 1000000.0 ) {
-    get<i>( guess ) = val;
-    const auto throughputs = network.throughputs_fast( get<0>( guess ),
-						       get<1>( guess ),
-						       get<2>( guess ) );
-    const double score = PCC_utility( get<i>( guess ), get<i>( throughputs ) );
-
-    if ( score > best_score ) {
-      best_score = score;
-      best_rates = guess;
+    if ( mid_score > low_score and mid_score > high_score ) {
+      //      cout << "Terminating search with multiplier = " << multiplier << "\n";
+      return guess;
+    } else if ( low_score > mid_score ) {
+      guess = low_guess;
+      delta *= 1.1;
+    } else {
+      guess = high_guess;
+      delta *= 1.1;
     }
   }
-
-  return best_rates;  
 }
 
 tuple<double, double, double> search( ParkingLot & network,
-				   const tuple<double, double, double> guess,
-				   const double search_radius )
+				      const tuple<double, double, double> guess,
+				      const double search_radius )
 {
   bool edge = true;
   tuple<double, double, double> best_throughputs { -1, -1, -1 };
@@ -128,59 +150,36 @@ tuple<double, double, double> search( ParkingLot & network,
   return best_throughputs;
 }
 
-void print( const tuple<double, double, double> & best_rates )
-{
-  cout << "** Best rates: " << get<0>( best_rates )
-       << " " << get<1>( best_rates )
-       << " " << get<2>( best_rates ) << "\n";  
-}
-
 int main()
 {
   ParkingLot network;
-  tuple<double, double, double> best_rates { 14, 4, 6 };
 
-  double scale = 20;
+  for ( double A = 0; A < 25; A += 0.5 ) {
+    for ( double B = 0; B < 25; B += 0.5 ) {
+      for ( double C = 0; C < 25; C += 0.5 ) {
+	tuple<double, double, double> best_rates { A, B, C };
 
-  while ( true ) {
-    print( best_rates );
-    cout << "Optimizing A.\n";
-    best_rates = search_one<0>( network, best_rates, scale );
+	for ( unsigned int i = 0; i < 100000; i++ ) {
+	  auto new_rates_A = search_one<0>( network, best_rates );
+	  auto new_rates_B = search_one<1>( network, best_rates );
+	  auto new_rates_C = search_one<2>( network, best_rates );
 
-    print( best_rates );
-    cout << "Optimizing B.\n";
-    best_rates = search_one<1>( network, best_rates, scale );
+	  auto new_rates = make_tuple( get<0>( new_rates_A ),
+				       get<1>( new_rates_B ),
+				       get<2>( new_rates_C ) );
 
-    print( best_rates );
-    cout << "Optimizing C.\n";
-    best_rates = search_one<2>( network, best_rates, scale );
+	  if ( new_rates == best_rates ) {
+	    break;
+	  }
 
-    scale *= 0.99;
+	  best_rates = new_rates;
+	}
+
+	cout << A << " " << B << " " << C << " -> ";
+	print( best_rates );
+      }
+    }
   }
-
-  /*
-  best_throughputs = search( network, best_throughputs, 15 );
-  cout << "Best throughputs after round 1: " << get<0>( best_throughputs )
-       << " " << get<1>( best_throughputs )
-       << " " << get<2>( best_throughputs ) << "\n";
-
-  best_throughputs = search( network, best_throughputs, 1.5f );
-  cout << "Best throughputs after round 2: " << get<0>( best_throughputs )
-       << " " << get<1>( best_throughputs )
-       << " " << get<2>( best_throughputs ) << "\n";
-
-  best_throughputs = search( network, best_throughputs, .15f );
-  cout << "Best throughputs after round 3: " << get<0>( best_throughputs )
-       << " " << get<1>( best_throughputs )
-       << " " << get<2>( best_throughputs ) << "\n";
-
-  best_throughputs = search( network, best_throughputs, .015f );
-  cout << "Best throughputs after round 4: " << get<0>( best_throughputs )
-       << " " << get<1>( best_throughputs )
-       << " " << get<2>( best_throughputs ) << "\n";
-  */
-
-  cout << "Successful audits of the fast calculation: " << network.audit_count() << "\n";
 
   return EXIT_SUCCESS;
 }
