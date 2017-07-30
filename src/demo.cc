105fb2d3c68a999b3f68a6b722ae91ae0e96f8f9
Keith Winstein <keithw@cs.stanford.edu>
2017-07-30 03:27:56 -0700

    Improve progress report

--- src/demo.cc
+++ src/demo.cc
@@ -19,11 +19,16 @@ int main()
   tuple<double, double, double> best_throughputs;
   double best_score = numeric_limits<double>::lowest();
   unsigned int calculation_count = 0;
+  int last_percent = -1;
 
-  for ( double A = 0; A < 20; A += .025 ) {
-    cerr << A << "/20\n";
-    for ( double B = 0; B < 20; B += .025 ) {
-      for ( double C = 0; C < 20; C += .025 ) {
+  for ( double A = 0; A < 30; A += .05 ) {
+    const int percent = int( 100 * A / 30 );
+    if ( percent != last_percent ) {
+      cerr << percent << "%\n";
+      last_percent = percent;
+    }
+    for ( double B = 0; B < 30; B += .05 ) {
+      for ( double C = 0; C < 30; C += .05 ) {
 	const auto throughputs = network.throughputs_fast( A, B, C, calculation_count++ );
 	const double score =
 	  utility( get<0>( throughputs ) )
